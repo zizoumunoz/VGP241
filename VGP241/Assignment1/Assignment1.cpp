c728232b465b07dc4e24a4dc54d756dec8b7d249
Zinadine Muñoz Luna <57816983+zizoumunoz@users.noreply.github.com>
2026-07-18 18:47:10 -0700

    Add Assignment1 project

---
+++ VGP241/Assignment1/Assignment1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <Array.h>
+#include <Vector.h>
+
+int main()
+{
+    std::cout << "Custom Containers\n";
+    // add necessary .h and .cpp files
+    // since its small, can fit it all in main
+    // using appropriate .h and .cpp helps practice formatting C++ code
+}
