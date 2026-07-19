6b98dda9bca3507ae11456e89ebb1233e9f2bbac
Zinadine Muñoz Luna <57816983+zizoumunoz@users.noreply.github.com>
2026-07-18 19:10:27 -0700

    Add Player class members

--- VGP241/Assignment1/Player.cpp
+++ VGP241/Assignment1/Player.cpp
@@ -1,1 +1,17 @@
 #include "Player.h"
+#include "Stats.h"
+
+void Player::initPlayer()
+{
+	mStats[Stats::Health] = 100;
+
+	for (size_t i = 1; i < mStats.Size(); i++)
+	{
+		mStats[i] = rand() % 5 + 1;
+	}
+}
+
+int Player::getStat(Stats stat) const
+{
+	return mStats[stat];
+}
--- VGP241/Assignment1/Player.h
+++ VGP241/Assignment1/Player.h
@@ -1,4 +1,21 @@
 #pragma once
+#include <string>
+#include <Array.h>
+#include "Stats.h"
+
 class Player
-{};
+{
+public:
+	Player() = default;
+
+	// Function to initialize player with default values
+	void initPlayer();
+
+	int getStat(Stats stat) const;
+
+private:
+	std::string mName = "";
+	Array<int, 5> mStats;
+
+};
 
