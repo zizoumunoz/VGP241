78fcd2197ee3e171f80915610ff378bee51776c4
Zinadine Muñoz Luna <57816983+zizoumunoz@users.noreply.github.com>
2026-07-18 19:36:17 -0700

    Add step 3d and 3e

--- VGP241/Assignment1/Assignment1.cpp
+++ VGP241/Assignment1/Assignment1.cpp
@@ -32,9 +32,41 @@ int main()
         players[i].initPlayer();
     }
 
+    // 3c. Display the names of all players and all of their stats
     for (size_t i = 0; i < players.Size(); i++)
     {
         players[i].display();
         std::cout << "\n";
     }
+
+    // 3d. Remove 6 players
+    for (size_t i = 0; i < 6; i++)
+    {
+        players.PopBack();
+    }
+
+    // 3e. Add players for a total of 20
+
+    players.PushBack(Player("Pubert"));
+    players.PushBack(Player("Anisa"));
+    players.PushBack(Player("Ethan"));
+    players.PushBack(Player("Martin"));
+    players.PushBack(Player("Dale"));
+
+    players.PushBack(Player("Dave"));
+    players.PushBack(Player("Steve"));
+    players.PushBack(Player("Matthew"));
+    players.PushBack(Player("Gilbert"));
+    players.PushBack(Player("Peter"));
+    players.PushBack(Player("Joe"));
+
+    players.PushBack(Player("Glenn"));
+    players.PushBack(Player("Meg"));
+    players.PushBack(Player("Stewart"));
+    players.PushBack(Player("Brian"));
+    players.PushBack(Player("Quahog"));
+
+    // 3f. Pick a random 2 players, compare their stats to see who would win in a fight
+
+
 }
