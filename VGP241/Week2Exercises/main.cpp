ff9439fb6ed0f75cb9c74e68e19e92f1de257ba5
Zinadine Muñoz Luna <57816983+zizoumunoz@users.noreply.github.com>
2026-07-25 22:45:38 -0700

    Add instructions for exercise

---
+++ VGP241/Week2Exercises/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <array>
+
+
+
+/*
+	Simple screen flow
+	Create three containers
+		Array
+		Vector
+		Stack
+	Create functions for each of them to
+		Go to a screen with an std::string screen
+		Go back
+	For each:
+		Go to main menu
+		Go to level setup
+		Go to game
+		Go to game over
+		Return to main menu by going back
+		print the time to process each type
+
+*/
+
+int main()
+{
+	return 0;
+}
