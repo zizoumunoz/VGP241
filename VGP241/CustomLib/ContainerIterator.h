a54132b40a91c69701edb398a7d2132226b90eb0
Zinadine Muñoz Luna <57816983+zizoumunoz@users.noreply.github.com>
2026-07-25 23:06:08 -0700

    Add ContainerIterator code

--- VGP241/CustomLib/ContainerIterator.h
+++ VGP241/CustomLib/ContainerIterator.h
@@ -19,4 +19,69 @@ public:
 	using difference_type = std::ptrdiff_t;
 	using pointer = T*;
 	using reference = T&;
+
+public:
+
+	ContainerIterator(T* ptr) : m_ptr(ptr) {}
+
+	T& operator*()	// star operator to get the address of something
+	{
+		return *m_ptr;
+	}
+
+	T* operator->()	// dereference operator to get the value of something a pointer points at
+	{
+		return m_ptr;
+	}
+
+	// ++iter
+	ContainerIterator& operator++()
+	{
+		++m_ptr;
+		return *this;
+	}
+
+	// --iter
+	ContainerIterator& operator--()
+	{
+		--m_ptr;
+		return *this;
+	}
+
+	// post increments
+	ContainerIterator operator++(int)
+	{
+		ContainerIterator tmp = *this;
+		++(*this);
+		return tmp;
+	}
+
+	ContainerIterator operator--(int)
+	{
+		ContainerIterator tmp = *this;
+		--(*this);
+		return tmp;
+	}
+
+	// (+ and -) does not modify the object
+	ContainerIterator operator+(difference_type n) const { return ContainerIterator(m_ptr + n); }
+	ContainerIterator operator-(difference_type n) const { return ContainerIterator(m_ptr - n); }
+
+	// (+= and -=) modifies the object and returns a reference
+	ContainerIterator& operator+=(difference_type n) { m_ptr += n; return *this; }
+	ContainerIterator& operator-=(difference_type n) { m_ptr -= n; return *this; }
+
+	difference_type operator-(const ContainerIterator& rhs) const { return m_ptr - rhs.m_ptr; }
+
+	// comparisons
+	bool operator==(const ContainerIterator& rhs) const { return m_ptr == rhs.m_ptr; }
+	bool operator!=(const ContainerIterator& rhs) const { return m_ptr != rhs.m_ptr; }
+	bool operator<(const ContainerIterator& rhs) const { return m_ptr < rhs.m_ptr; }
+	bool operator>(const ContainerIterator& rhs) const { return m_ptr > rhs.m_ptr; }
+	bool operator<=(const ContainerIterator& rhs) const { return m_ptr <= rhs.m_ptr; }
+	bool operator>=(const ContainerIterator& rhs) const { return m_ptr >= rhs.m_ptr; }
+
+private:
+	T* m_ptr = nullptr;
+
 };
